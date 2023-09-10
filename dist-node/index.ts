4a14d9d82f056845641604bfe8408226bf8a51ab
percy chuzon <percyrondan1995@gmail.com>
2023-09-10 16:29:33 -0500

    se agrego un paquete ts en cada archivo para que no genere error en las importaciones

---
+++ dist-node/index.ts
@@ -0,0 +1,45 @@
+
+/* function para barajar los arrays */
+export function ShuffleX(arrayX: any[], limit: number): any[] {
+  let ctr = arrayX.length;
+  let temp: any;
+  let index: number;
+
+  // While there are elements in the array
+  while (ctr > 0) {
+    // Pick a random index
+    index = Math.floor(Math.random() * ctr);
+    // Decrease ctr by 1
+    ctr--;
+    // And swap the last element with it
+    temp = arrayX[ctr];
+    arrayX[ctr] = arrayX[index];
+    arrayX[index] = temp;
+  }
+  return arrayX.slice(0, limit);
+}
+
+
+/* function para generar id */
+export function ShortIdx(limit: number = 7): string {
+  const character: string[] = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '-', '_' ];
+
+  let shuffleArray = ShuffleX(character, limit);
+
+  let result = shuffleArray.join("").slice(0, limit);
+
+  return result;
+}
+
+
+/* function para generar id con símbolos util para contraseñas */
+export function RandomIdx(limit: number = 7): string {
+  const character: string[] =[ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '-', '_', '!', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~' ];
+
+  let shuffleArray = ShuffleX(character, limit);
+
+  let result = shuffleArray.join("").slice(0, limit);
+
+  return result;
+}
+
